7b7de23cf2abde8a1db264f1ac68e5a56e0175cd
Joe Fairburn <Joe.fairburn1@gmail.com>
2020-06-25 12:19:29 +0100

    Revert "Updates"
    This reverts commit d2e6ee9028fd7b1b5b17b2071d3592fd42c7025a.

---
+++ src/images/typetest.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function TypeTestText(props) {
+
+  return (
+    <div>
+        This website was created as part of a research project, looking at how to improve users typing speed in typing speed tests. The 
+    </div>
+  )
+}
+
+export default TypeTestText
